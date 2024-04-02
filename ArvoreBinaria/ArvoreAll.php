ff01f1afa4b9d5331a6de10438a8c30f21835036
Fernando Pavão <70675152+fernando-LP@users.noreply.github.com>
2024-04-02 20:17:55 -0300

    Update ArvoreAll.php

--- ArvoreBinaria/ArvoreAll.php
+++ ArvoreBinaria/ArvoreAll.php
@@ -13,6 +13,31 @@ class Node
         $this->esquerda = null;
     }
 }
+
+class PesquisaBinaria extends ArvoreBinaria
+{
+    public function inserir($valorInserir)
+    {
+        $rasNoPai = null;
+        $x = $this->root;
+        while ($x !== null) {
+            $rasNoPai = $x;
+            if ($valorInserir < $x->data) {
+                $x = $x->esquerda;  #Recebe valor a esquerda da arvore
+            } else {
+                $x = $x->direita; # Recebe valor a direita da arvore
+            }
+        }
+        if ($rasNoPai === null) {
+            $this->root = new Node($valorInserir);
+        } elseif ($valorInserir < $rasNoPai->data) {
+            $rasNoPai->esquerda = new Node($valorInserir);
+        } else {
+            $rasNoPai->direita = new Node($valorInserir);
+        }
+    }
+}
+
 class ArvoreBinaria
 {
     public $root; ## Armazena nó raiz
@@ -174,29 +199,6 @@ class ArvoreBinaria
             }
         }*/
 }
-class PesquisaBinaria extends ArvoreBinaria
-{
-    public function inserir($valorInserir)
-    {
-        $rasNoPai = null;
-        $x = $this->root;
-        while ($x !== null) {
-            $rasNoPai = $x;
-            if ($valorInserir < $x->data) {
-                $x = $x->esquerda;  #Recebe valor a esquerda da arvore
-            } else {
-                $x = $x->direita; # Recebe valor a direita da arvore
-            }
-        }
-        if ($rasNoPai === null) {
-            $this->root = new Node($valorInserir);
-        } elseif ($valorInserir < $rasNoPai->data) {
-            $rasNoPai->esquerda = new Node($valorInserir);
-        } else {
-            $rasNoPai->direita = new Node($valorInserir);
-        }
-    }
-}
 
 
 
@@ -314,4 +316,3 @@ do {
     }
 
 } while ($opcao != 14);
-
